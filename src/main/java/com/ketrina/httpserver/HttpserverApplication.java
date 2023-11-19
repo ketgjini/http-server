6500ef0f1cde5c15a04f85dab36006c7f8bf2c0a
Programuesja <gjiniketrina@gmail.com>
2023-11-19 02:37:01 +0100

    added documentation for rest endpoints

--- src/main/java/com/ketrina/httpserver/HttpserverApplication.java
+++ src/main/java/com/ketrina/httpserver/HttpserverApplication.java
@@ -1,11 +1,13 @@
 package com.ketrina.httpserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition
 public class HttpserverApplication {
 
 	public static void main(String[] args) {
